bfa099a777e6584cabbb35adc61d2bf73932a41f
Toni Agne <toniagne@gmail.com>
2015-09-14 01:15:13 -0300

    ERRO

--- js/app.js
+++ js/app.js
@@ -24,8 +24,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
 })
 
 .config(function($stateProvider, $urlRouterProvider, $compileProvider) {
-
- $compileProvider.aHrefSanitizationWhitelist(/^\s*(whatsapp):/);
+  
   // Ionic uses AngularUI Router which uses the concept of states
   // Learn more here: https://github.com/angular-ui/ui-router
   // Set up the various states which the app can be in.
